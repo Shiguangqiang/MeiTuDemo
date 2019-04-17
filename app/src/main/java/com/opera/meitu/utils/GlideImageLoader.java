bf6c54d18e18563a80e6c01901966c05124d38ee
Sgq <guangqiang.shi@wkzuche.com>
2019-04-17 16:36:57 +0800

    更新Glide,添加混淆

--- app/src/main/java/com/opera/meitu/utils/GlideImageLoader.java
+++ app/src/main/java/com/opera/meitu/utils/GlideImageLoader.java
@@ -4,14 +4,13 @@ import android.content.Context;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.opera.meitu.R;
 import com.youth.banner.loader.ImageLoader;
 
 public class GlideImageLoader extends ImageLoader {
     @Override
     public void displayImage(Context context, Object path, ImageView imageView) {
         //Glide 加载图片简单用法
-        Glide.with(context).load(path).into(imageView);
-
+        Glide.with(context).load(path).centerCrop().placeholder(R.mipmap.ic_launcher).into(imageView);
     }
-
 }
