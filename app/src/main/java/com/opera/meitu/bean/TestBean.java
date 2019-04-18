c4dfb5be6dccc1f1b196e9e961c31eabb1d53236
Sgq <guangqiang.shi@wkzuche.com>
2019-04-18 15:12:36 +0800

    fix testbean

--- app/src/main/java/com/opera/meitu/bean/TestBean.java
+++ app/src/main/java/com/opera/meitu/bean/TestBean.java
@@ -6,4 +6,31 @@ package com.opera.meitu.bean;
  * desc: 测试bean
  */
 public class TestBean {
+
+    private String name;
+    private String uid;
+
+    @Override
+    public String toString() {
+        return "TestBean{" +
+                "name='" + name + '\'' +
+                ", uid='" + uid + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name == null ? "" : name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUid() {
+        return uid == null ? "" : uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
 }
