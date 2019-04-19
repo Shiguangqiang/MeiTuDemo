c71e739e3a30025d0656bbad09c4e32f7c75a6e1
Sgq <guangqiang.shi@wkzuche.com>
2019-04-19 17:34:57 +0800

    fix glide on android p bugs

--- app/src/main/java/com/opera/meitu/utils/GlideImageLoader.java
+++ app/src/main/java/com/opera/meitu/utils/GlideImageLoader.java
@@ -11,6 +11,10 @@ public class GlideImageLoader extends ImageLoader {
     @Override
     public void displayImage(Context context, Object path, ImageView imageView) {
         //Glide 加载图片简单用法
-        Glide.with(context).load(path).centerCrop().placeholder(R.mipmap.ic_launcher).into(imageView);
+        Glide.with(context)
+                .load(path)
+                .centerCrop()
+                .placeholder(R.mipmap.ic_launcher)
+                .into(imageView);
     }
 }
